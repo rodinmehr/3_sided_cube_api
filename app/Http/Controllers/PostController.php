93387c1a057b1f8b7bd4f9d9eccf7877e8128342
Mahsa Radinmehr <rodinmehr@gmail.com>
2024-07-22 09:15:18 +0100

    add OpenAPI documentation for other post API endpoints

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,6 +23,38 @@ class PostController extends Controller
         $this->middleware('auth:sanctum')->only(['store', 'update', 'destroy']);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/posts",
+     *     summary="Show all posts",
+     *     tags={"Posts"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the post",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="title", type="string"),
+     *                 @OA\Property(property="content", type="string"),
+     *                 @OA\Property(property="author", type="string"),
+     *                 @OA\Property(property="created_at", type="string", format="date-time"),
+     *                 @OA\Property(property="updated_at", type="string", format="date-time")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Post not found"
+     *     )
+     * )
+     */
     // public function index(): JsonResponse
     public function index(): AnonymousResourceCollection
     {
@@ -31,6 +63,36 @@ class PostController extends Controller
         return PostResource::collection($posts);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/posts",
+     *     summary="Create a new post",
+     *     tags={"Posts"},
+     *     security={ {"sanctum": {}} },
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="title", type="string"),
+     *                 @OA\Property(property="content", type="string"),
+     *                 @OA\Property(property="author", type="string"),
+     *                 @OA\Property(property="created_at", type="string", format="date-time"),
+     *                 @OA\Property(property="updated_at", type="string", format="date-time")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error"
+     *     ),
+     * )
+     */
     // public function store(PostRequest $request): JsonResponse
     public function store(PostRequest $request): PostResource
     {
@@ -78,6 +140,52 @@ class PostController extends Controller
         return new PostResource($post);
     }
 
+    /**
+     * @OA\Put(
+     *     path="/api/posts/{id}",
+     *     summary="Update a specific post",
+     *     tags={"Posts"},
+     *     security={ {"sanctum": {}} },
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the post",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *    @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"title", "content", "author"},
+     *             @OA\Property(property="title", type="string"),
+     *             @OA\Property(property="content", type="string"),
+     *             @OA\Property(property="author", type="string"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="title", type="string"),
+     *                 @OA\Property(property="content", type="string"),
+     *                 @OA\Property(property="author", type="string"),
+     *                 @OA\Property(property="created_at", type="string", format="date-time"),
+     *                 @OA\Property(property="updated_at", type="string", format="date-time")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error"
+     *     ),
+     * )
+     */
     // public function update(PostRequest $request, Post $post): JsonResponse
     public function update(PostRequest $request, Post $post): PostResource
     {
@@ -86,6 +194,33 @@ class PostController extends Controller
         return new PostResource($post);
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/posts/{id}",
+     *     summary="Delete a specific post",
+     *     tags={"Posts"},
+     *     security={ {"sanctum": {}} },
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the post",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Successful operation",
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error"
+     *     ),
+     * )
+     */
     public function destroy(Post $post): JsonResponse
     {
         $post->delete();
